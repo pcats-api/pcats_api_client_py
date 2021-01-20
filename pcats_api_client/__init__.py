d151cb769dbd41cdc6e4f4665315b8abc70f0e3f
Michal Kouril <xmkouril@gmail.com>
2021-01-19 23:49:56 -0500

    Exported _cate functions

--- pcats_api_client/__init__.py
+++ pcats_api_client/__init__.py
@@ -1,1 +1,1 @@
-from .pcats_api import job_status, staticgp, dynamicgp, uploadfile, wait_for_result, print, ploturl
+from .pcats_api import job_status, staticgp, dynamicgp, uploadfile, wait_for_result, print, ploturl, staticgp_cate, dynamicgp_cate
